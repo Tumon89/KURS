08852197389f3cf32876680ce63c4772a5d693fb
Tumon89 <tumon89@gmail.com>
2020-04-15 23:54:37 +0300

    задание 5

---
+++ src/zadanie_5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class zadanie_5 {
+    public static void main(String agrs[]) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число для умножения:");
+        String numb = in.nextLine();
+        int i = Integer.parseInt(numb);
+        System.out.println(i*1);
+        System.out.println(i*2);
+        System.out.println(i*3);
+        System.out.println(i*4);
+        System.out.println(i*5);
+        System.out.println(i*6);
+        System.out.println(i*7);
+        System.out.println(i*8);
+        System.out.println(i*9);
+        System.out.println(i*10);
+    }
+}
